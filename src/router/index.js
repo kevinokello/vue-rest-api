b9b88c15c03bd0d54cc709ae3f6e6e661b76fb7f
kevinokello <okellokevin77@gmail.com>
2022-10-07 11:23:47 +0300

    project setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/skills",
+      name: "SkillIndex",
+      component: () => import("../views/skills/SkillIndex.vue"),
+    },
+    {
+      path: "/skills/create",
+      name: "SkillCreate",
+      component: () => import("../views/skills/SkillCreate.vue"),
+    },
+    {
+      path: "/skills/:id/edit",
+      name: "SkillEdit",
+      component: () => import("../views/skills/SkillEdit.vue"),
+      props: true,
+    },
+  ],
+});
 
-export default router
+export default router;
